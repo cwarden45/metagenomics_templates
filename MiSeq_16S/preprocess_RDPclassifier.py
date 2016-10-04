98989426d5b35497133fd53763ba04eb6dde55ed
Charles Warden <cwarden45@gmail.com>
2016-10-04 16:38:40 -0700

    Add files via upload

---
+++ MiSeq_16S/preprocess_RDPclassifier.py
@@ -0,0 +1,122 @@
+import sys
+import re
+import os
+from Bio import SeqIO
+
+parameterFile = "parameters.txt"
+finishedSamples = []
+
+threads = ""
+mem = ""
+quantFolder = ""
+readsFolder = ""
+RDPclassifier = ""
+
+inHandle = open(parameterFile)
+lines = inHandle.readlines()
+			
+for line in lines:
+	print line
+	line = re.sub("\n","",line)
+	line = re.sub("\r","",line)
+	
+	lineInfo = line.split("\t")
+	param = lineInfo[0]
+	value = lineInfo[1]
+
+	if param == "Reads_Folder":
+		readsFolder = value
+	
+	if param == "Classification_Folder":
+		quantFolder = value
+		
+	if param == "Java_Mem":
+		mem = value
+		
+	if param == "Threads":
+		threads = value
+		
+	if param == "RDPclassifier_Jar":
+		RDPclassifier = value
+		
+if (RDPclassifier== "") or (RDPclassifier == "[required]"):
+	print "Need to enter a value for 'RDPclassifier_Jar'!"
+	sys.exit()
+		
+if (threads == "") or (threads == "[required]"):
+	print "Need to enter a value for 'Threads'!"
+	sys.exit()
+	
+if (mem == "") or (mem == "[required]"):
+	print "Need to enter a value for 'Java_Mem'!"
+	sys.exit()
+
+if (readsFolder == "") or (readsFolder == "[required]"):
+	print "Need to enter a value for 'Reads_Folder'!"
+	sys.exit()
+	
+if (quantFolder == "") or (quantFolder == "[required]"):
+	print "Need to enter a value for 'Classification_Folder'!"
+	sys.exit()
+
+mergedReadsFolder = readsFolder + "/PEAR_Merged"
+if not os.path.exists(mergedReadsFolder):
+	command = "mkdir " + mergedReadsFolder
+	os.system(command)
+	
+fileResults = os.listdir(readsFolder)
+for file in fileResults:
+	result = re.search("(.*)_L\d{3}_R1_001.fastq$",file)
+	if result:
+		command = "gzip " + file
+		os.system(command)
+		file = file + ".gz"
+	
+	resultGZ = re.search("(.*)_L\d{3}_R1_001.fastq.gz$",file)
+	
+	if resultGZ:
+		sample = resultGZ.group(1)
+		if sample not in finishedSamples:
+			print sample
+			
+			classificationFolder = quantFolder + "/" + sample
+			if not os.path.exists(classificationFolder):
+				command = "mkdir " + classificationFolder
+				os.system(command)
+			
+			read1 = readsFolder + "/" + file
+			read2 = re.sub("R1_001.fastq","R2_001.fastq",read1)
+								
+			print "\n\nMerge Reads using PEAR\n\n"
+			pearPrefix = mergedReadsFolder + "/" + sample
+			command = "pear -f " + read1 + " -r " + read2 + " -o " + pearPrefix + " -j " + threads
+			os.system(command)
+
+			print "\n\nFASTQ to FASTA (and create read length table)\n\n"
+			pearAssembly = pearPrefix + ".assembled.fastq"
+			rdpInput = pearPrefix + ".assembled.fasta"
+			readLengthFile = pearPrefix + ".assembled.read_length"
+			
+			outHandle = open(readLengthFile, 'w')
+			text = "Merged.Read\tLength\n"
+			outHandle.write(text)
+			
+			fastq_parser = SeqIO.parse(pearAssembly, "fastq")
+			SeqIO.write(fastq_parser, rdpInput, "fasta")
+
+			for fastq in fastq_parser:
+				readName = fastq.id
+				readLength = len(str(fastq.seq))
+				
+				text = readName + "\t" +str(readLength)+ "\n"
+				outHandle.write(text)
+			
+			command = "gzip " + pearAssembly
+			os.system(command)
+			
+			print "\n\nApply RDPclassifier\n\n"
+			treeOut = classificationFolder + "/PEAR.RDP.fastahier.txt"
+			percentOut = classificationFolder + "/PEAR.RDP.out"
+			command = "java -jar -Xmx" + mem + " " + RDPclassifier + " -o " + percentOut + " -h " + treeOut + " " + rdpInput
+			os.system(command)
+
