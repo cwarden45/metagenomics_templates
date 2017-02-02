e7aeca544c3715bc69c7b7a76ee05d95b731ede2
Charles Warden <cwarden45@gmail.com>
2017-02-02 14:32:05 -0800

    Add files via upload

---
+++ PacBio_16S/RDP_BWA_filter_seqs.py
@@ -0,0 +1,18 @@
+import os
+
+rdp = "/mnt/user_data/Seq/cwarden/rdp_classifier_2.11/dist/classifier.jar"
+
+fullFA = "trainset15_092015.fa"
+filterFA1 = "trainset15_092015.rm.dupseq.fa"
+command = "java -Xmx2g -jar " + rdp + " rm-dupseq -d -l 1200 -i " + fullFA + " -o " + filterFA1
+os.system(command)
+
+filterFA2 = "trainset15_092015.rm.partialseq.fa"
+command = "java -Xmx8g -jar " + rdp + " rm-partialseq " + filterFA1+ " " + filterFA1 + " " + filterFA2
+os.system(command)
+
+command = "/opt/bwa/bwa index " filterFA2
+os.system(command)
+
+command = "/opt/ncbi-blast-2.4.0+/bin/makeblastdb -max_file_sz 2GB -in " + filterFA2 + " -dbtype nucl"
+os.system(command)
