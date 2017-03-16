9e20b84c2f46e3868d445a8277cdf3fa958c1a22
Charles Warden <cwarden45@gmail.com>
2017-03-16 10:22:21 -0700

    Add files via upload

--- MiSeq_16S/merged_length_QC_stats.R
+++ MiSeq_16S/merged_length_QC_stats.R
@@ -35,18 +35,20 @@ if ((classifier == "RDPclassifier")|(classifier == "BWA")){
 		target.files = file.path(merged.read.folder,fileID[targetID == target])
 		target.files = paste(target.files, ".assembled.read_length",sep="")
 		target.sample = shortID[targetID == target]
-		target.groups = groupID[targetID == target]
-		
-		group.levels = levels(as.factor(target.groups))
-		num.sample.types = length(group.levels)
-		color.palette = fixed.color.palatte[1:num.sample.types]
-
-		labelColors = rep("black",times=length(target.files))
-		for (i in 1:length(target.groups)){
-			labelColors[target.groups == as.character(group.levels[i])] = color.palette[i]
-		}#end for (i in 1:length(target.groups))
 		
 		for (group in plot.groups){
+			groupID = as.character(sample.table[,group])
+			target.groups = groupID[targetID == target]
+			
+			group.levels = levels(as.factor(target.groups))
+			num.sample.types = length(group.levels)
+			color.palette = fixed.color.palatte[1:num.sample.types]
+
+			labelColors = rep("black",times=length(target.files))
+			for (i in 1:length(target.groups)){
+				labelColors[target.groups == as.character(group.levels[i])] = color.palette[i]
+			}#end for (i in 1:length(target.groups))
+			
 			dist.file  = paste(target,"_PEAR_merged_length_dist_by_",group,".png",sep="")
 			png(dist.file)
 			for (i in 1:length(target.files)){
@@ -87,7 +89,6 @@ if ((classifier == "RDPclassifier")|(classifier == "BWA")){
 	fileID = as.character(sample.table$sampleID)
 	shortID = as.character(sample.table$userID)
 	targetID = as.character(sample.table$Target)
-	groupID = as.character(sample.table$Group)
 	
 	length.upper5= rep(NA, length(fileID))
 	length.upper.quantile = rep(NA, length(fileID))
@@ -104,37 +105,42 @@ if ((classifier == "RDPclassifier")|(classifier == "BWA")){
 		target.files = file.path(merged.read.folder,fileID[targetID == target])
 		target.files = paste(target.files, ".trim.contigs.good.read_length",sep="")
 		target.sample = shortID[targetID == target]
-		target.groups = groupID[targetID == target]
-		
-		group.levels = levels(as.factor(target.groups))
-		num.sample.types = length(group.levels)
-		color.palette = fixed.color.palatte[1:num.sample.types]
-
-		labelColors = rep("black",times=length(target.files))
-		for (i in 1:length(target.groups)){
-			labelColors[target.groups == as.character(group.levels[i])] = color.palette[i]
-		}#end for (i in 1:length(target.groups))
 		
 		for (group in plot.groups){
+			groupID = as.character(sample.table[,group])
+			target.groups = groupID[targetID == target]
+			
+			group.levels = levels(as.factor(target.groups))
+			num.sample.types = length(group.levels)
+			color.palette = fixed.color.palatte[1:num.sample.types]
+
+			labelColors = rep("black",times=length(target.files))
+			for (i in 1:length(target.groups)){
+				labelColors[target.groups == as.character(group.levels[i])] = color.palette[i]
+			}#end for (i in 1:length(target.groups))
+			
 			dist.file  = paste(target,"_mothur_contig_length_dist_by_",group,".png",sep="")
 			png(dist.file)
 			for (i in 1:length(target.files)){
-				target.table = read.table(target.files[i],sep="\t", head=T)
-				length.quantiles = quantile(target.table$Length, c(0.05,0.25,0.5,0.75, 0.95))
-				length.lower5[shortID == target.sample[i]] = length.quantiles[1]
-				length.lower.quantile[shortID == target.sample[i]] = length.quantiles[2]
-				length.med[shortID == target.sample[i]] = length.quantiles[3]
-				length.upper.quantile[shortID == target.sample[i]] = length.quantiles[4]
-				length.upper5[shortID == target.sample[i]] = length.quantiles[5]
-				length.mean[shortID == target.sample[i]] = mean(length.quantiles)
-				
-				den <- density(target.table$Length, na.rm=T, from=xrange[1], to=xrange[2])
-				if(i == 1){
-					plot(den$x, den$y, type="l", xlab ="Merged Length", main = target,
-							ylab = "Density",col=labelColors[i], ylim = yrange)
-				}else{
-					lines(den$x, den$y, type = "l", col=labelColors[i], new=FALSE)
-				}#end else
+				if(file.exists(target.files[i])){
+					print(target.files[i])
+					target.table = read.table(target.files[i],sep="\t", head=T)
+					length.quantiles = quantile(target.table$Length, c(0.05,0.25,0.5,0.75, 0.95))
+					length.lower5[shortID == target.sample[i]] = length.quantiles[1]
+					length.lower.quantile[shortID == target.sample[i]] = length.quantiles[2]
+					length.med[shortID == target.sample[i]] = length.quantiles[3]
+					length.upper.quantile[shortID == target.sample[i]] = length.quantiles[4]
+					length.upper5[shortID == target.sample[i]] = length.quantiles[5]
+					length.mean[shortID == target.sample[i]] = mean(length.quantiles)
+					
+					den <- density(target.table$Length, na.rm=T, from=xrange[1], to=xrange[2])
+					if(i == 1){
+						plot(den$x, den$y, type="l", xlab ="Merged Length", main = target,
+								ylab = "Density",col=labelColors[i], ylim = yrange)
+					}else{
+						lines(den$x, den$y, type = "l", col=labelColors[i], new=FALSE)
+					}#end else
+				}#end if(file.exists(target.files[i]))
 			}#end for (i in 1:length(target.files))
 			legend("topleft",legend=group.levels, col=color.palette, lwd=2)
 			dev.off()
